ec4a1667e8e607bfa7d04c6ef37d305b17cca1ca
supermads <madisonsfahey@gmail.com>
2020-12-15 11:34:23 -0800

    Stage 3 of Matrix Project

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -1,32 +1,82 @@
 def add_matrices():
-    an, am = input().split()
-    an = int(an)
-    am = int(am)
     A = []
     B = []
+    an, am = map(int, input("Enter size of first matrix: ").split())
+    print("Enter first matrix: ")
     for i in range(0, an):
         A.append(input().split())
-    bn, bm = input().split()
-    bn = int(bn)
-    bm = int(bm)
+    bn, bm = map(int, input("Enter size of second matrix: ").split())
+    print("Enter second matrix: ")
     for i in range(0, bn):
         B.append(input().split())
     if an == bn and am == bm:
-        C = [[int(A[n][m]) + int(B[n][m]) for m in range(len(A[0]))] for n in range(len(A))]
+        C = [[float(A[n][m]) + float(B[n][m]) for m in range(len(A[0]))] for n in range(len(A))]
+        print("The result is:")
         for i in range(0, len(C)):
             print(" ".join(map(str, C[i])))
     else:
-        print("ERROR")
+        print("The operation cannot be performed.")
         
 
-def multiply_matrix():
-    an, am = input().split()
+def multiply_matrix_by_constant():
     A = []
+    an, am = input("Enter size of matrix: ").split()
+    print("Enter matrix:")
     for i in range(0, int(an)):
         A.append(input().split())
-    scalar = int(input())
-    C = [[int(A[n][m]) * scalar for m in range(len(A[0]))] for n in range(len(A))]
-    print(C)
+    scalar = float(input("Enter constant: "))
+    C = [[float(A[n][m]) * scalar for m in range(len(A[0]))] for n in range(len(A))]
+    print("The result is:")
     for i in range(0, len(C)):
         print(" ".join(map(str, C[i])))
 
+
+def multiply_matrices():
+    A = []
+    B = []
+    an, am = map(int, input("Enter size of first matrix: ").split())
+    print("Enter first matrix: ")
+    for i in range(0, an):
+        A.append(input().split())
+    bn, bm = map(int, input("Enter size of second matrix: ").split())
+    print("Enter second matrix: ")
+    for i in range(0, bn):
+        B.append(input().split())
+    if am == bn:
+        C = [[0 for _i in range (bm)] for _j in range(an)]
+        for n in range(an):
+            for m in range(bm):
+                acc = 0
+                for x in range(am):
+                    acc += float(A[n][x]) * float(B[x][m])
+                C[n][m] = acc
+        print("The result is:")
+        for i in range(0, len(C)):
+            print(" ".join(map(str, C[i])))
+    else:
+        print("The operation cannot be performed")
+
+
+def print_menu():
+    print("1. Add matrices")
+    print("2. Multiply matrix by a constant")
+    print("3. Multiply matrices")
+    print("0. Exit")
+
+
+def main():
+    print_menu()
+    choice = int(input("Your choice: "))
+    while choice != 0:
+        if choice == 1:
+            add_matrices()
+        elif choice == 2:
+            multiply_matrix_by_constant()
+        elif choice == 3:
+            multiply_matrices()
+        print_menu()
+        choice = int(input("Your choice: "))
+
+
+
+main()
